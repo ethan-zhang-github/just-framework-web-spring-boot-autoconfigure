9765db45ae11f9d35af40468f232cfe5ed504e18
JUST1984 <yixiezi1994@gmail.com>
2019-09-09 09:28:09 +0800

    update

--- src/main/java/pers/just1984/framework/web/autoconfigure/configuration/JustThreadPoolConfiguration.java
+++ src/main/java/pers/just1984/framework/web/autoconfigure/configuration/JustThreadPoolConfiguration.java
@@ -2,6 +2,9 @@ package pers.just1984.framework.web.autoconfigure.configuration;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.autoconfigure.endpoint.condition.ConditionalOnEnabledEndpoint;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import pers.just1984.framework.web.autoconfigure.component.endpoint.JustThreadPoolEndpoint;
 import pers.just1984.framework.web.autoconfigure.configuration.property.JustThreadPoolProperties;
 
 import java.util.concurrent.Executor;
@@ -55,6 +59,13 @@ public class JustThreadPoolConfiguration {
                 return executor;
             }
         };
+    }
+
+    @ConditionalOnClass(Health.class)
+    @ConditionalOnEnabledEndpoint
+    @Bean
+    public JustThreadPoolEndpoint justThreadPoolEndpoint() {
+        return new JustThreadPoolEndpoint();
     }
 
 }
